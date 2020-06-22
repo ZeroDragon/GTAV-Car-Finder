6eca3d8178e7b72ffed36e9d2e8b6453d1dc1181
Jorge Medrano <medrano.jorge@gmail.com>
2020-06-22 15:16:59 -0500

    funcion /level (#1)
    * Se agrego la funcion /level para mostar el XP por nivel del rango 1 al 999
    
    * se borro un console.log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api')
 const cars = require('./carritos.json')
+const levels = require('./levels.json')
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true})
 
 bot.onText(
@@ -21,6 +22,23 @@ bot.onText(
     const car = cars[name]
     let message = 'no car found with that name'
     if (car) message = `${ name } | ${ car.hex }\n\`\`\` ${ car.data } \`\`\` `,
+    await bot.sendMessage(
+      chatId,
+      message,
+      { parse_mode: 'Markdown' }
+    )
+  }
+)
+
+bot.onText(
+  /^\/level (.+)$|^\/level$/i,
+  async ({ chat: { id: chatId}}, [, levelFind = 1]) => {
+    let message = 'no level found with that number'
+    if ((~~levelFind === parseInt(levelFind, 10) && (levelFind > 0 && levelFind <= levels.length))) {
+      const numberLevel = levelFind
+      const level = levels[numberLevel - 1]
+      message = `RANK XP ${ numberLevel } = \`\`\` ${ level } \`\`\` `
+    }
     await bot.sendMessage(
       chatId,
       message,
